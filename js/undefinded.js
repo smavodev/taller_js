5b67ec1292879f51abde7120cce38177402fb65e
Sergio Villagomez <43839357+smavo@users.noreply.github.com>
2021-02-17 22:47:00 -0500

    add files, Temas: Booleans, Undefined, null and NaN

---
+++ js/undefinded.js
@@ -0,0 +1,23 @@
+
+  // **********     Curso JavaScript: 9. undefined, null & NaN  ********** */
+
+    // undefined indica que no se ha inicializado una variable y que el valor está ausente
+    let indefinida ;
+    //console.log(indefinida);
+
+    //null es un valor especial que indica la ausencia de un valor
+    let nulo = null;
+    //console.log(null);
+
+    //NaN - Not a Number
+    let noEsUnNumero = "hola" * 3.7;
+    //console.log(noEsUnNumero);
+
+    let Mayuscula = null;
+    let C = 7.19;
+    //console.log(C + Number.parseInt(Mayuscula));
+
+//  Una variable a la que no se le ha asignado valor, 
+//  o no se ha declarado en absoluto (no se declara, no existe) son de tipo undefined. 
+//  Un método o sentencia también devuelve undefined si la variable que se está evaluando no tiene asignado un valor. 
+//  Una función devuelve undefined si no se ha devuelto un valor.
